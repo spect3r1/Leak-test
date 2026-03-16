c9279f26a1e73f8660d64886d314410d5b207eb2
Spect3r <spect3r@kali.Kali>
2026-03-16 22:18:48 +0200

    Add nested secret leak PoC

---
+++ deploy.py
@@ -0,0 +1,37 @@
+import os
+import logging
+
+from verda.containers import (
+    Deployment,
+    Container,
+    ComputeResource,
+    ContainerRegistrySettings,
+    DockerHubCredentials,
+)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+registry_creds = DockerHubCredentials(
+    name="prod-registry",
+    username=os.environ["DOCKERHUB_USERNAME"],
+    access_token=os.environ["DOCKERHUB_TOKEN"],
+)
+
+deployment = Deployment(
+    name="prod-api",
+    containers=[
+        Container(
+            name="api",
+            image="docker.io/acme/private-api:latest",
+            exposed_port=8080,
+        )
+    ],
+    compute=ComputeResource(vcpu=2, memory=4),
+    container_registry_settings=ContainerRegistrySettings(
+        is_private=True,
+        credentials=registry_creds,
+    ),
+)
+
+logger.info("Creating deployment: %s", deployment)
